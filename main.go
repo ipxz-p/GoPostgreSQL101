9031014cbdd63256a1c90a95091f33529fe80365
PongsiriSuwannasilp <64070193@it.kmitl.ac.th>
2024-10-03 16:03:55 +0700

    go routine

--- main.go
+++ main.go
@@ -2,135 +2,58 @@ package main
 
 import (
 	"fmt"
-	"log"
-  "os"
-  "time"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-	// "database/sql"
-	// "fmt"
-	// "log"
-	// "strconv"
+	"sync"
+
 	"github.com/gofiber/fiber/v2"
-  "github.com/joho/godotenv"
-  "github.com/golang-jwt/jwt/v4"
-	// _ "github.com/lib/pq"
 )
 
-const (
-  host     = "localhost"  // or the Docker service name if running in another container
-  port     = 5432         // default PostgreSQL port
-  user     = "myuser"     // as defined in docker-compose.yml
-  password = "mypassword" // as defined in docker-compose.yml
-  dbname   = "mydatabase" // as defined in docker-compose.yml
-)
+type Message struct {
+  Data string `json:"data"`
+}
 
-func requireAuth(c *fiber.Ctx) error {
-  cookie := c.Cookies("jwt")
+type Pubsub struct {
+  subs []chan Message
+  mu sync.Mutex
+}
 
-  token, err := jwt.ParseWithClaims(cookie, &jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
-    return []byte(os.Getenv("JWT_KEY")), nil
-  })
-  if err != nil || !token.Valid {
-    return c.SendStatus(fiber.StatusUnauthorized)
+func (ps *Pubsub) Subscribe() chan Message {
+  ps.mu.Lock()
+  defer ps.mu.Unlock()
+  ch := make(chan Message, 1)
+  ps.subs = append(ps.subs, ch)
+  return ch
+}
+
+func (ps *Pubsub) Punlish(msg *Message) {
+  ps.mu.Lock()
+  defer ps.mu.Unlock()
+  for _, sub := range ps.subs {
+    sub <- *msg
   }
-
-  return c.Next()
 }
 
 func main() {
-  err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
-  dsn := fmt.Sprintf("host=%s port=%d user=%s "+
-  "password=%s dbname=%s sslmode=disable",
-  host, port, user, password, dbname)
-
-  newLogger := logger.New(
-    log.New(os.Stdout, "\r\n", log.LstdFlags),
-    logger.Config{
-      SlowThreshold: time.Second,
-      LogLevel:      logger.Info,
-      Colorful:      true,
-    },
-  )
-
-  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-    Logger: newLogger,
-  })
-
-  if err != nil {
-    panic("failed to connect to database")
-  }
-
-  db.AutoMigrate(&Book{}, &Publisher{}, &Author{}, &AuthorBook{})
-
-  publisher := Publisher{
-    Details: "Publisher Details",
-    Name:    "Publisher Name",
-  }
-  _ = createPublisher(db, &publisher)
-
-  author := Author{
-    Name: "Author Name",
-  }
-  _ = createAuthor(db, &author)
-
-  book := Book{
-    Name:        "Book Title",
-    Author:      "Book Author",
-    Description: "Book Description",
-    PublisherID: publisher.ID,
-    Authors:     []Author{author},
-  }
-  _ = createBookWithAuthor(db, &book, []uint{author.ID})
   app := fiber.New()
 
-  // app.Use("/books", requireAuth)
-  // app.Get("/books", func(c *fiber.Ctx) error {
-  //   return c.JSON(getBooks(db))
-  // })
-  // app.Post("/reg", func(c *fiber.Ctx) error {
-  //   user := new(User)
+  pubsub := &Pubsub{}
 
-  //   if err := c.BodyParser(user); err != nil {
-  //     return c.SendStatus(fiber.StatusBadRequest)
-  //   }
-
-  //   err = createUser(db, user)
-
-  //   if err != nil {
-  //     return c.SendStatus(fiber.StatusBadRequest)
-  //   }
-
-  //   return c.JSON(fiber.Map{
-  //     "message": "Reg success",
-  //   })
-  // })
-
-  // app.Post("/login", func(c *fiber.Ctx) error {
-  //   user := new(User)
-  //   if err := c.BodyParser(user); err != nil {
-  //     return c.SendStatus(fiber.StatusBadRequest)
-  //   }
-  //   token, err := loginUser(db, user)
-  //   if err != nil {
-  //     return c.SendStatus(fiber.StatusBadRequest)
-  //   }
-
-  //   c.Cookie(&fiber.Cookie{
-  //     Name: "jwt",
-  //     Value: token,
-  //     Expires: time.Now().Add(time.Hour * 72),
-  //     HTTPOnly: true,
-  //   })
-
-  //   return c.JSON(fiber.Map{
-  //     "token": "Login success",
-  //   })
-  // })
+  app.Post("/publisher", func(c *fiber.Ctx) error {
+    message := new(Message);
+    if err := c.BodyParser(message); err != nil {
+      return c.SendStatus(fiber.StatusBadRequest)
+    }
+    pubsub.Punlish(message)
+    return c.JSON(fiber.Map{
+      "message": "add to subscriber",
+    }) 
+  })
+  
+  sub := pubsub.Subscribe()
+  go func ()  {
+    for msg := range sub {
+      fmt.Println("Receive message: ", msg)
+    }
+  }()
 
   app.Listen(":8080")
 }
